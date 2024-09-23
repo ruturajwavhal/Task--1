09f8a0947261e15cde85425f0b53197e15f50ecd
Ruturaj <99982142+ruturajwavhal@users.noreply.github.com>
2024-09-23 18:00:32 +0530

    Delete pb_3.py

--- pb_3.py
+++
@@ -1,6 +0,0 @@
-Rollno = int(input("Enter the Roll No. : "))
-Name = str(input("Enter your First Name : "))
-Percentage = float(input("Enter the Percentage : "))
-
-print("Rollno \tName \tPercentage")
-print(Rollno,Name,Percentage,sep="\t")
